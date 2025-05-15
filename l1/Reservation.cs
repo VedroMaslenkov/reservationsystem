f020b8f8f070098bfcfffa0b7527f1e512cb2328
VedroMaslenkov <nektarnirvana@gmail.com>
2025-05-15 23:16:25 +0300

    Добавлен класс Reservation

---
+++ l1/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum ReservationStatus
+{
+    Активно,
+    Отменено,
+    Завершено
+}
+public class Reservation
+{
+    public string CustomerName { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public ReservationStatus Status { get; set; }
+    public Reservation(string customerName, DateTime startTime, DateTime endTime)
+    {
+        CustomerName = customerName;
+        StartTime = startTime;
+        EndTime = endTime;
+        Status = ReservationStatus.Активно;
+    }
+    public void UpdateStatus(ReservationStatus newStatus)
+    {
+        Status = newStatus;
+    }
+}
+
